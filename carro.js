eb2a9f121103265c06f77cdd57af15637de78928
Yonak97 <angeldavidosmapanplona@gmail.com>
2024-12-06 15:02:53 -0500

    carrito

--- carro.js
+++ carro.js
@@ -32,5 +32,29 @@ productsList.addEventListener('click', e => {
 
         }
 
+        allProducts = [...allProducts, infoProduct];
+
+        showHTML();
 	}
+
 });
+
+//fundion mostrar html
+
+const showHTML =() => {
+
+    allProducts.forEach(product =>{
+		const containerProduct = document.createElement('div');
+        containerProduct.classList.add('cart-product');
+
+		containerProduct.innerHTML = `
+            <div class="info-cart-product">
+                <span class="cantidad-producto-carrito">${product.quantity}</span>
+                <p class="titulo-producto-carrito">${product.title}</p>
+            </div>
+                <img class="icon-close" src="../images/icon-close.png" alt="icon close">
+        `
+
+        rowProduct.append(containerProduct)
+    } )
+}
