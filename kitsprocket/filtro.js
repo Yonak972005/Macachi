ed85cf4b362c7db7d673fd6e5d2af71e7edcdd4b
Yonak97 <angeldavidosmapanplona@gmail.com>
2024-12-23 13:37:06 -0500

    medidas y filtros

--- kitsprocket/filtro.js
+++ kitsprocket/filtro.js
@@ -12,3 +12,27 @@ document.addEventListener("keyup", e=>{
         })
     }
 })
+
+document.addEventListener("DOMContentLoaded", function() {
+    const products = document.querySelectorAll('.product');
+
+    products.forEach((product) => {
+        const mainImage = product.querySelector('img'); // Imagen principal del producto
+        const medidasImage = product.querySelector('.medidas-image'); // Imagen de las medidas
+
+        // Función para manejar el clic en la imagen principal
+        mainImage.addEventListener('click', function() {
+            // Alternar visibilidad de la imagen de medidas
+            if (medidasImage.style.display === 'none' || medidasImage.style.display === '') {
+                medidasImage.style.display = 'block'; // Mostrar la imagen de medidas
+            } else {
+                medidasImage.style.display = 'none'; // Ocultar la imagen de medidas
+            }
+        });
+
+        // Función para manejar el clic en la imagen de medidas para cerrarla
+        medidasImage.addEventListener('click', function() {
+            medidasImage.style.display = 'none'; // Ocultar la imagen de medidas
+        });
+    });
+});
